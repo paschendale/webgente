dac493d8ceef63e88e519275c15309e9722ea725
Victor Marotta <38250078+paschendale@users.noreply.github.com>
2020-01-30 16:02:07 -0300

    Update de quadras em Jacobina

--- conf_jacobina/overlay.js
+++ conf_jacobina/overlay.js
@@ -33,23 +33,100 @@ var vetorOverlay = [
         tiled: true
 	},
 
-	hidrografia = {
-		nome: 'Hidrografia',
-		grupo: 'Infraestrutura Urbana',
-		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
-		maxZoom: 25,
-        layers: 'jacobina:Hidrografia',
-        format: 'image/png',
-        transparent: true,
-        tiled: true
-	},
-
 	eixo_via = {
 		nome: 'Eixos de Via',
 		grupo: 'Cadastro Urbano',
 		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
 		maxZoom: 25,
         layers: 'jacobina:Eixo_Via',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	arvore = {
+		nome: 'Arvores',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:arvore',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	bl = {
+		nome: 'Bocas de Lobo e Bueiros',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:bl',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	lixeira = {
+		nome: 'Lixeiras',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:lixeira',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	placa = {
+		nome: 'Placas',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:placa',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	pontobus = {
+		nome: 'Pontos de Onibus',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:pontobus',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	poste = {
+		nome: 'Postes',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:pontobus',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	pv = {
+		nome: 'Poços de Visitação',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:pv',
+        format: 'image/png',
+        transparent: true,
+        tiled: true
+	},
+
+	telpubl = {
+		nome: 'Telefones Públicos',
+		grupo: 'Infraestrutura Urbana',
+		host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
+		maxZoom: 25,
+        layers: 'jacobina:telpubl',
         format: 'image/png',
         transparent: true,
         tiled: true
