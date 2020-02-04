3827b940961d81cda771870f4d6bacd00ae557d3
Victor Marotta <38250078+paschendale@users.noreply.github.com>
2020-02-04 16:20:58 -0300

    Adicao automatica de camadas usando o leaflet.wms

--- js/mapa/main.js
+++ js/mapa/main.js
@@ -7,24 +7,15 @@ function main(){
 	myMapa = new mapa(init.latitude,init.longitude,init.zoomInicial);
 	myMapa.scale();
 
-    /*var optionsControl = {
+    var optionsControl = {
         collapsed: true
     };
     
     Lc = L.control.groupedLayers(null, null, optionsControl).addTo(myMapa.getMapa());
 
-    k = 0; // Inicializa contador
-    var camadaOverlay = [0]; // Inicializa vetor externo à função para armazenamento das camadas de Overlay 
-
-    function adicionaOverlay(objeto){
-        k++;
-        camadaOverlay[k] = new wmsCamada(objeto);
-        Lc.addOverlay(camadaOverlay[k].getLayer(),objeto.nome,objeto.grupo);
-    }
-
     i = 0; // Inicializa contador
     var camadaBase = [0]; // Inicializa vetor externo à função para armazenamento das camadas base
- 
+	 
     function adicionaBasemap(objeto){
         i++;
         camadaBase[i] = new wmsCamada(objeto)
@@ -33,30 +24,23 @@ function main(){
     }
 
     vetorBasemap.forEach(adicionaBasemap);
-    vetorOverlay.forEach(adicionaOverlay);
-    */
-
-    parametros_cbge_quadras = {
-        nome: 'Quadras CTM',
-        grupo: 'Cadastro Urbano',
-        host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
-        maxZoom: 25,
-        layers: 'bomdespacho:CBGE_Quadras',
-        format: 'image/png',
-        transparent: true,
-        tiled: true
-    }
-
-    /*L.tileLayer.wms('https://geoserver.genteufv.com.br/geoserver/ows?', parametros_cbge_quadras).addTo(myMapa.getMapa());*/
-
 
     var source = L.WMS.source("https://geoserver.genteufv.com.br/geoserver/ows?", {
           opacity: 1,
           tiled: true,
           maxZoom: 25,
-          "info_format": "text/html"
+          "info_format": "text/html",
+          transparent: true,
+          format: 'image/png'
     });
     
-    source.getLayer("bomdespacho:pontos_levantamento_360").addTo(myMapa.getMapa());
+    var camadaOverlay = [0]; // Inicializa vetor externo à função para armazenamento das camadas de Overlay 
+	k = 0; // Inicializa contador
+    function adicionaSourceOverlay (objeto){
+    	k++;
+    	camadaOverlay[k] = source.getLayer(objeto.layers);
+    	Lc.addOverlay(camadaOverlay[k],objeto.nome,objeto.grupo);
+    };
 
+    vetorOverlay.forEach(adicionaSourceOverlay)
 }
