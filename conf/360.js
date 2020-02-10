90385555ee50b8225cb35746fae8d215c2845be6
Victor Marotta <38250078+paschendale@users.noreply.github.com>
2020-02-10 20:29:51 -0300

    Adicionado comentarios e otimizado o codigo

--- conf/360.js
+++ conf/360.js
@@ -1,4 +1,7 @@
-parametros_pontos_levantamento_360 = {
+// O levantamento 360, caso exista, deverá ter um atributo chamado 'Caminho', com o caminho da imagem dentro de algum servidor disponibilizado na web
+// a camada deverá ser configurada dentro da variável levantamento_360. Não podendo ser alterado o nome.
+
+levantamento_360 = {
 		nome: 'Imagens 360°',
 		grupo: 'Cadastro Urbano',
 		tipo: '360',
--- js/mapa/main.js
+++ js/mapa/main.js
@@ -1,28 +1,34 @@
- //Função principal int main()
-var opt_gfi = 1;
+// Cria uma variável global para setar o tipo de informação a ser puxada no GetFeatureInfo, 1: Abre visualizador 360, 2: Abre tabela de atributos
+var opt_gfi = 2;
 
+//Função principal int main()
 function main(){
 
-    //Variável global para controle da exibição ou não da "Barra de Edição" confome o clique do usuário;
+    //Variável global para controle da exibição ou não da "Barra de Edição" conforme o clique do usuário;
     barraEdicaoHabilitado = false;
+
+    // Inicializa o mapa conforme configurações explicitadas em conf/startup.js
 
 	myMapa = new mapa(init.latitude,init.longitude,init.zoomInicial);
 	myMapa.scale();
 
-    // Adicionando alguns botoes
+    // Adicionando botão de set view para visao inicial
 
     var homeBut = L.easyButton('<img src="img/home.png">', function(btn, map){
         var initial = [init.latitude,init.longitude];
         map.setView(initial,init.zoomInicial);
     }).addTo(myMapa.getMapa());
 
-    // Fim da adição de botoes
+    // Cria o controle de camadas
 
     var optionsControl = {
-        collapsed: true
+        collapsed: true,
+        groupCheckboxes: true
     };
     
     Lc = L.control.groupedLayers(null, null, optionsControl).addTo(myMapa.getMapa());
+
+    // Adiciona automaticamente todas as camadas definidas em conf/base.js
 
     i = 0; // Inicializa contador
     var camadaBase = [0]; // Inicializa vetor externo à função para armazenamento das camadas base
@@ -36,6 +42,10 @@ function main(){
 
     vetorBasemap.forEach(adicionaBasemap);
 
+    // Cria o source, inicializado pelo plugin Leaflet.wms para a adição das camadas overlay, 
+    // o primeiro parametro é definido em conf/overlay.js, especificando o host do GeoServer de onde serão puxadas as camadas
+    // o segundo parâmetro são as opções do plugin, especificando a requisição WMS e o GetFeatureInfo
+
     var source = L.WMS.source(overlayHost, {
           opacity: 1,
           tiled: true,
@@ -45,48 +55,46 @@ function main(){
           format: 'image/png'
     });
     
+    // Adiciona automaticamente todas as camadas definidas em conf/overlay.js
+
     var camadaOverlay = [0]; // Inicializa vetor externo à função para armazenamento das camadas de Overlay 
 	k = 0; // Inicializa contador
 
     function adicionaSourceOverlay (objeto){
     	k++;
-        if (objeto.tipo == 'camada'){
-            camadaOverlay[k] = source.getLayer(objeto.layers);
-            Lc.addOverlay(camadaOverlay[k],objeto.nome,objeto.grupo);
-        }
-    };
-
-    function adiciona360 (objeto){
-        k++;
-        if (objeto.tipo == '360'){
-            camadaOverlay[k] = source.getLayer(objeto.layers).addTo(myMapa.getMapa());
-        }
+        camadaOverlay[k] = source.getLayer(objeto.layers);
+        Lc.addOverlay(camadaOverlay[k],objeto.nome,objeto.grupo);
     };
 
     vetorOverlay.forEach(adicionaSourceOverlay);
 
-    var camada360 = source.getLayer(parametros_pontos_levantamento_360.layers);
+    // Testa se existe a camada do levantamento 360, definida dentro do arquivo conf/360.js, caso exista, carrega o botão de habilitar a camada e o visualizador 360.
 
-    var estado = L.easyButton({
-        states: [{
-                stateName: '360_disabled',
-                icon:      '<img src="img/360-degree.png">',
-                title:     'Liga o visualizador de imagens 360°',   
-                onClick: function(btn, map) {       
-                    opt_gfi = 1;
-                    camada360.addTo(myMapa.getMapa());
-                    estado.state('360_enabled');    
-                }
-            }, {
-                stateName: '360_enabled',   
-                icon:      '<img src="img/360-degree-clicked.png">',               
-                title:     'Desativa o visualizador de imagens 360°',
-                onClick: function(btn, map) {
-                    opt_gfi = 2;
-                    myMapa.getMapa().removeLayer(camada360);
-                    estado.state('360_disabled');
-                }
-        }]
-    });
-    estado.addTo(myMapa.getMapa());
+	if (typeof levantamento_360 != 'undefined') {
+    	var camada360 = source.getLayer(levantamento_360.layers);
+
+	    var estado = L.easyButton({
+	        states: [{
+	                stateName: '360_disabled',
+	                icon:      '<img src="img/360-degree.png">',
+	                title:     'Liga o visualizador de imagens 360°',   
+	                onClick: function(btn, map) {       
+	                    opt_gfi = 1;
+	                    camada360.addTo(myMapa.getMapa());
+	                    estado.state('360_enabled');    
+	                }
+	            }, {
+	                stateName: '360_enabled',   
+	                icon:      '<img src="img/360-degree-clicked.png">',               
+	                title:     'Desativa o visualizador de imagens 360°',
+	                onClick: function(btn, map) {
+	                    opt_gfi = 2;
+	                    myMapa.getMapa().removeLayer(camada360);
+	                    estado.state('360_disabled');
+	                }
+	        }]
+	    });
+	    estado.addTo(myMapa.getMapa());
+	};
+   
 }
