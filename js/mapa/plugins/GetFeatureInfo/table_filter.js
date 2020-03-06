50909cce551ea20c97d82ba7b0f2e4486019310b
TavaresCarlos <caca_cht@hotmail.com>
2020-03-06 17:07:49 -0300

    Requisição wfs para recuperar json com as propriedades dos setores

--- js/mapa/plugins/GetFeatureInfo/table_filter.js
+++ js/mapa/plugins/GetFeatureInfo/table_filter.js
@@ -6,9 +6,12 @@ function filtros(){
 	var quadra = vetorOverlay[2];
 
 	quadra.host = overlayHost;
-
 	quadra.cql_filter = cql_setor.getAttribute("name") +"="+ cql_setor.value;// +" and "+cql_quadra.getAttribute("name") +"= "+ cql_quadra.value ;
-	
+
+	console.log(quadra);
+
+	//Chamada da camada wms do geoserver como imagem
+
 	if(controle == true){
 		myMapa.getMapa().removeLayer(quadra_filtro.getLayer());
 		controle = false;
@@ -18,6 +21,34 @@ function filtros(){
 		quadra_filtro.getLayer().addTo(myMapa.getMapa());
 		controle = true;
 	}
+
+	//Chamada de todos os atributos da camada wms requisitada
+
+	var defaultParameters = {
+        service : 'WFS',
+        version : '1.0.0',
+        request : 'GetFeature',
+        typeName : quadra.layers,
+        outputFormat : 'text/javascript',
+        format_options : 'callback:getJson',
+        SrsName : 'EPSG:4326'
+    };
+
+    var parameters = L.Util.extend(defaultParameters);
+    var URL = "https://geoserver.genteufv.com.br/geoserver/ows" + L.Util.getParamString(parameters);
+
+    var xhr = $.ajax({
+        url: URL,
+        dataType: 'jsonp', 
+        cache: false, 
+        jsonpCallback: 'getJson',
+        	success: function(response){
+        		//Editar o json de resposta das quadras dos setores aqui em um popup
+            	for(var i=0; i<response.features.length; i++){
+            		console.log(response.features[i].properties);
+            	}
+            }
+    	});
 }
 
 function tabela () {
