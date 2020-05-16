046866eeb2e19f823c3d20a0fcc3fea1eabf0ac6
sabrin577 <60109306+sabrin577@users.noreply.github.com>
2020-05-16 15:08:11 -0300

    aplicação jqueryMask e validate

---
+++ js/menu-principal/aplicacaoJMask.js
@@ -0,0 +1,42 @@
+$(document).ready(function(){
+	$("#cpf").mask("000.000.000-00");
+	$("#usuario").mask("000.000.000-00", {reverse: true});
+	$("#celular").mask("(00) 00000-0009");
+	$("#celular").blur(function(event){
+		if($(this).val().length==15){
+			$("#celular").mask("(00) 00000-0000");
+		}else{
+			$("#celular").mask("(00) 0000-0000");
+		}
+	});
+
+	//Validação de campos
+$("#formulario").validate({
+		rules:{
+			nome: {
+				required: true,
+				maxlength:100,
+				minlength:5
+			},
+
+			email: {
+				required: true,
+				email: true
+			},
+
+			cpf: {
+				required: true,
+				cpfBR:true
+			} ,
+			senha: {
+				required: true,
+				maxlength:20,
+				minlength:4
+			}
+},
+		submitHandler: function(form){
+			alert("Cadastro concluído!")
+		} 
+	})	
+
+})
