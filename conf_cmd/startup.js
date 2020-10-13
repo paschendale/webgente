41eb07e8a52c4113abc519fb14859b016f2e3b82
Victor Marotta <victordossantosmarotta@gmail.com>
2020-10-13 14:21:24 -0300

    removendo pastas conf de outros projetos

--- conf_cmd/startup.js
+++
@@ -1,9 +0,0 @@
-// Definindo coordenada e zoom inicial do WebGIS
-var init = {
-	latitude: -19.031712, 
-	longitude: -43.423265, 
-	zoomInicial: 18
-};
-
-
-
--- conf_jacobina/startup.js
+++
@@ -1,7 +0,0 @@
-// Definindo coordenada e zoom inicial do WebGIS
-var init = {
-	latitude: -11.184880, 
-	longitude: -40.510286, 
-	zoomInicial: 17
-};
-
