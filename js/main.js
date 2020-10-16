387cd87c09b799d165a8760756b5077f72fa8470
Victor Marotta <victordossantosmarotta@gmail.com>
2020-10-16 11:24:52 -0300

    Arrumando main e index.html

--- js/main.js
+++ js/main.js
@@ -9,48 +9,9 @@ var mde= "";
 //Função principal int main()
 function main(){
 
-    //L.DomEvent.disableClickPropagation(L.DomUtil.get('consultaPesquisa'));
-    L.DomEvent.disableScrollPropagation(L.DomUtil.get('consultaPesquisa'));
-
-    //Variável global para controle da exibição ou não da "Barra de Edição" conforme o clique do usuário;
-    barraEdicaoHabilitado = false;
-
-    // Inicializa o mapa conforme configurações explicitadas em conf/startup.js
-
-	myMapa = new mapa(init.latitude,init.longitude,init.zoomInicial);
-	myMapa.scale();
-
-    // Adicionando botão de set view para visao inicial
-
-    var homeBut = L.easyButton('<img src="img/home.png">', function(btn, map){
-        var initial = [init.latitude,init.longitude];
-        map.setView(initial,init.zoomInicial);
-    },'Voltar o mapa à vista inicial').addTo(myMapa.getMapa());
-
-
-    //Adicionando o botao de visualizar informacoes com dois estados
-
-    var informacao = L.easyButton({
-        states: [{
-                    stateName: 'information_disabled',
-                    icon:      '<img src="img/information_active.png">',
-                    title:     'Habilita a ferramenta de visualização de informações das camadas',   
-                    onClick: function(btn) {       
-                        informacao.state('information_enabled');
-                        btn.state('information_enabled');  
-                        info_gfi=true;  
-                    }
-                }, {
-                    stateName: 'information_enabled',   
-                    icon:      '<img src="img/information_desactive.png">',               
-                    title:     'Desabilita a ferramenta de visualização de informações das camadas',
-                    onClick: function(btn) {
-                        informacao.state('information_disabled');
-                        btn.state('information_disabled'); 
-                        info_gfi=false;   
-                    }
-            }]
-        }).addTo(myMapa.getMapa());
+    // Inicializa o mapa conforme configurações em conf/startup.js
+    myMapa = new mapa(init.latitude,init.longitude,init.zoomInicial);
+    myMapa.scale();
 
     var optionsControl = {
         collapsed: true,
@@ -75,7 +36,7 @@ function main(){
     vetorBasemap.forEach(adicionaBasemap);
 
     // Cria o source, inicializado pelo plugin Leaflet.wms para a adição das camadas overlay, 
-    // o primeiro parametro é definido em conf/overlay-, especificando o host do GeoServer de onde serão puxadas as camadas
+    // o primeiro parametro é definido em conf/anon/overlay.js, especificando o host do GeoServer de onde serão puxadas as camadas
     // o segundo parâmetro são as opções do plugin, especificando a requisição WMS e o GetFeatureInfo
 
     var source = L.WMS.source(overlayHost, {
@@ -85,44 +46,81 @@ function main(){
           "info_format": "application/json",
           transparent: true,
           format: 'image/png'
-    });
-    
-    // Adiciona automaticamente todas as camadas definidas em conf/overlay.js
+    });    
+
 
     var camadaOverlay = [0]; // Inicializa vetor externo à função para armazenamento das camadas de Overlay 
-	k = 0; // Inicializa contador
+    k = 0; // Inicializa contador
 
     function adicionaSourceOverlay (objeto){
-    	k++;
+        k++;
         camadaOverlay[k] = source.getLayer(objeto.layers);
         objeto.alpha=[];
         Lc.addOverlay(camadaOverlay[k], objeto.nome, objeto.grupo);
     };
 
     vetorOverlay.forEach(adicionaSourceOverlay);  
-   // mde= source.getLayer(vetorMDE[0].layers);
+
+    /* -------------- Controles de Ferramentas ------------------ */
+
+    // Adicionando botão de set view para visao inicial
+    var homeBut = L.easyButton('<img src="img/home.png">', function(btn, map){
+        var initial = [init.latitude,init.longitude];
+        map.setView(initial,init.zoomInicial);
+    },'Voltar o mapa à vista inicial').addTo(myMapa.getMapa());
+
+    //Adicionando o botao de visualizar informacoes com dois estados
+    var informacao = L.easyButton({
+        states: [{
+                    stateName: 'information_disabled',
+                    icon:      '<img src="img/information_active.png">',
+                    title:     'Habilita a ferramenta de visualização de informações das camadas',   
+                    onClick: function(btn) {       
+                        informacao.state('information_enabled');
+                        btn.state('information_enabled');  
+                        info_gfi=true;  
+                    }
+                }, {
+                    stateName: 'information_enabled',   
+                    icon:      '<img src="img/information_desactive.png">',               
+                    title:     'Desabilita a ferramenta de visualização de informações das camadas',
+                    onClick: function(btn) {
+                        informacao.state('information_disabled');
+                        btn.state('information_disabled'); 
+                        info_gfi=false;   
+                    }
+            }]
+        }).addTo(myMapa.getMapa());
+
     // Cria botao para ativar a ferramenta de pesquisas
-
-
     var pesquisas = L.easyButton('<img src="img/lupa.png">', function(){
         barraPesquisas();
     },'Habilitar ferramenta de pesquisa por atributo nas camadas').addTo(myMapa.getMapa());
+    
+    // 
+    L.DomEvent.disableScrollPropagation(L.DomUtil.get('consultaPesquisa'));
 
-    // Cria botao para ativar as ferramentas de desenho
-
+    //Variável global para controle da exibição ou não da barra de ferramentas conforme o clique do usuário;
+    barraEdicaoHabilitado = false;
+    
+    // 
     var ferramentas = L.easyButton('<img src="img/engineer.png">', function(){
         barraEdicao();
     },'Habilitar ferramentas de desenho no mapa').addTo(myMapa.getMapa());
 
+    // 
     var exportar = L.easyButton('<img src="img/donwload.png">', function(btn, map){
         exportarMapa();
     },'Download do mapa em tela').addTo(myMapa.getMapa());
-     
+
+    //
     var minhaLocalização = L.easyButton('<img src="img/location.png">', function(btn, map){
         localizacao();
     },'Minha localização').addTo(myMapa.getMapa());
 
-  /*  var coordenadas = L.easyButton({
+
+    // mde= source.getLayer(vetorMDE[0].layers);
+    /*  var coordenadas = L.easyButton({
         states: [{
                     stateName: 'coordenadas_disabled',
                     icon:      '<img src="img/coordenadas_active.png">',
@@ -146,5 +144,5 @@ function main(){
                     }
             }]
         }).addTo(myMapa.getMapa());
-*/
+    */
 }
