fc4d0367220a71185b0564aa085067bda0cf9330
guirosemberg <68906555+guirosemberg@users.noreply.github.com>
2020-11-05 08:37:26 -0300

    Comentários em conf

--- conf/anon/base.js
+++ conf/anon/base.js
@@ -1,6 +1,17 @@
-// Definindo camadas base
+// Definindo camadas base do WebGENTE
+
+/* A variável abaixo foi criada para que quando for chamada em main.js o programa busque no host (servidor) os dados das camadas 
+de Imgeamento Aéreo e Orbital e Base Cadastral. O WebGENTE utiliza como servidor de mapas o GeoServer, que é um software de código livre e 
+compatível com as especificações WMS, WCS e WFS */
+
+// Web Map Service (WMS), em portugês serviço de mapa pela internet.
+// Web Coverage Service (WCS), em portugês serviço de cobertura pela internet.
+// Web Feature Service (WFS), em portugês serviço de cacacterísticas pela internet. 
+
 var vetorBasemap = [
     
+    //Parâmetro utilizado para carregar as informações da camada de Imageamento Aéreo e Orbital.
+
     parametros_imageamento = {
         nome: 'Imageamento Aéreo e Orbital',
         host: 'https://geoserver.genteufv.com.br/geoserver/ows?',
@@ -12,6 +23,8 @@ var vetorBasemap = [
         tiled: true,
         attribution: 'Imageamento realizado por aerofotogrametria, executado pelo Grupo de Engenharia para Gestão Territorial - GENTE em 2020'
     },
+
+    //Parâmetro para carregas as informações da camada de Base Cadastral
 
     parametros_cadastro = {
         nome: 'Base Cadastral',
@@ -25,6 +38,8 @@ var vetorBasemap = [
         attribution: 'Mapa Base do Cadastro Territorial elaborado pelo GENTE em 2020'
     }
 ];
+
+/* Variável criada para chamar as informações do Modelo Digital de Elevação no host */
 
 var vetorMDE = [
     parametros_mde = {
